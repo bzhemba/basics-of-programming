e98f4ec34c745bb38f216f1641918dd7a82d120a
Mariia <113040756+bzhemba@users.noreply.github.com>
2022-11-12 10:41:47 +0300

    Create lab7.1.c

---
+++ lab7.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+enum Weekday{
+    MONDAY = 1,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY
+};
+
+int main() {
+    printf("%d", MONDAY);
+    return 0;
+}
