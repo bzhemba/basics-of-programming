351dd0fe697819cbbc68e3c899d15765e4cd9366
Mariia <113040756+bzhemba@users.noreply.github.com>
2023-03-04 11:20:18 +0300

    Delete lab1 directory

--- lab1/Circle.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by мария баженова on 18.02.2023.
-//
-
-#include "Circle.h"
--- lab1/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-#include "main2.h"
-
-int main(){
-    isCorrect();
-    return 0;
-}
